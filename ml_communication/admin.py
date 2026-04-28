3a18bbab16c2762cf7eced33091bf5f62d217466
licenselesshunter <eliashm1998@gmail.com>
2026-04-28 11:10:51 -0400

    Se me olvido agregar el nuevo model en sección de amin, ahora está agregado

--- ml_communication/admin.py
+++ ml_communication/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import registered_order, ml_credentials, api_error
+from .models import registered_order, ml_credentials, api_error, ml_messages
 
 # Register your models here.
 admin.site.register(ml_credentials)
 admin.site.register(registered_order)
 admin.site.register(api_error)
+admin.site.register(ml_messages)
